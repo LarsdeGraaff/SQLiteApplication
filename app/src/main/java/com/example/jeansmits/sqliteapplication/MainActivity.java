9118df1b4b9473df3508471afb94f08a8d5e7755
LarsdeGraaff <lars.dg@gmail.com>
2015-09-10 10:53:53 +0200

    adapter getAllMovies works

--- app/src/main/java/com/example/jeansmits/sqliteapplication/MainActivity.java
+++ app/src/main/java/com/example/jeansmits/sqliteapplication/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.jeansmits.sqliteapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +17,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RecyclerView recyclerView= (RecyclerView) findViewById(R.id.r_view_movies);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+
+
+        MySQLiteHelper db = new MySQLiteHelper(this);
+
+        /**
+         * CRUD Operations (Create Read Update Delete)
+         * */
+        // add Movies
+        db.addMovie(new Movie("Android Application Development Cookbook", "Wei Meng Lee"));
+        db.addMovie(new Movie("Android Programming: The Big Nerd Ranch Guide", "Bill Phillips and Brian Hardy"));
+        db.addMovie(new Movie("Learn Android App Development", "Wallace Jackson"));
+
+        // get all movies
+        List<Movie> list = db.getAllMovies();
+
+        MovieAdapter movieAdapter= new MovieAdapter(list,this);
+        recyclerView.setAdapter(movieAdapter);
+
+        // delete one movie
+        //db.delete(list.get(0));
+
+        // get all movies
+        //db.getAllMovies();
+
+
     }
 
     @Override
